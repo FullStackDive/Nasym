9e21d7392b222c569747e84b9026cdbe81b09c6d
Mustafa <148890539+FullStackDive@users.noreply.github.com>
2026-09-26 22:12:33 +0300

    fix: delegate media permissions to JaaS

--- next.config.mjs
+++ next.config.mjs
@@ -17,6 +17,15 @@ const nextConfig = {
   async headers() {
     return [
       {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Permissions-Policy",
+            value: 'camera=(self "https://8x8.vc"), microphone=(self "https://8x8.vc"), display-capture=(self "https://8x8.vc"), fullscreen=(self "https://8x8.vc")',
+          },
+        ],
+      },
+      {
         source: "/fonts/:path*",
         headers: [
           { key: "Cache-Control", value: "public, max-age=31536000, immutable" }
